d9979ca3ff91ad64c86edfa230c1b76d88d22faa
gisa chris <gisachris24@gmail.com>
2023-06-21 10:42:48 -0200

    add redux folder setup

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  categories : [],
+  status : "under Construction"
+}
+
+const categories = createSlice({
+  name : 'category',
+  initialState,
+  reducers : {
+    SetStatus : (state,action) => {
+      state.status = action.payload
+    }
+  }
+})
+
+export default categories.reducer
+export const { SetStatus } = categories.actions;
